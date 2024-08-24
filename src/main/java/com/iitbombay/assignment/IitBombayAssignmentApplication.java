b16cefe8e81197d8fc91d5b1e70025f390764304
Atik Shaikh <mohammedatik1111@gmail.com>
2024-08-24 17:53:24 +0530

    first commit - all files added

---
+++ src/main/java/com/iitbombay/assignment/IitBombayAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.iitbombay.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IitBombayAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IitBombayAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iitbombay/assignment/IitBombayAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iitbombay.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IitBombayAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
